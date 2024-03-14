a8cca2a276106e38673aad5bc0859ac7aa528cb1
rkynbyw <akselrkyn@gmail.com>
2024-03-14 15:02:29 +0700

    Mini Task Web API Article & Category

---
+++ MyRESTServices/Controllers/ArticlesController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWebFormApp.BLL.DTOs;
+using MyWebFormApp.BLL.Interfaces;
+
+namespace MyRESTServices.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ArticlesController : ControllerBase
+    {
+        private readonly IArticleBLL _articleBLL;
+        public ArticlesController(IArticleBLL articleBLL)
+        {
+            _articleBLL = articleBLL;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var articles = _articleBLL.GetArticleWithCategory();
+                return Ok(articles);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("category/{categoryId}")]
+        public IActionResult GetByCategory(int categoryId)
+        {
+            try
+            {
+                var articles = _articleBLL.GetArticleByCategory(categoryId);
+                return Ok(articles);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                var article = _articleBLL.GetArticleById(id);
+                return Ok(article);
+            }
+            catch (ArgumentException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Insert([FromBody] ArticleCreateDTO article)
+        {
+            try
+            {
+                _articleBLL.Insert(article);
+                return Ok("Article created successfully");
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] ArticleUpdateDTO article)
+        {
+            try
+            {
+                article.ArticleID = id;
+                _articleBLL.Update(article);
+                return Ok("Article updated successfully");
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _articleBLL.Delete(id);
+                return Ok("Article deleted successfully");
+            }
+            catch (ArgumentException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+
+    }
+}
---
+++ MyRESTServices/Controllers/CategoriesController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWebFormApp.BLL.DTOs;
+using MyWebFormApp.BLL.Interfaces;
+
+namespace MyRESTServices.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryBLL _categoryBLL;
+        public CategoriesController(ICategoryBLL categoryBLL)
+        {
+            _categoryBLL = categoryBLL;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var categories = _categoryBLL.GetAll();
+                return Ok(categories);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                var category = _categoryBLL.GetById(id);
+                return Ok(category);
+            }
+            catch (ArgumentException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("search")]
+        public IActionResult GetByName([FromQuery] string name)
+        {
+            try
+            {
+                var categories = _categoryBLL.GetByName(name);
+                return Ok(categories);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Insert([FromBody] CategoryCreateDTO category)
+        {
+            try
+            {
+                _categoryBLL.Insert(category);
+                return Ok(category);
+                //return CreatedAtAction(nameof(GetById), new { id = category.CategoryID }, category);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] CategoryUpdateDTO category)
+        {
+            try
+            {
+                category.CategoryID = id;
+                _categoryBLL.Update(category);
+                return NoContent();
+            }
+            catch (ArgumentException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _categoryBLL.Delete(id);
+                return NoContent();
+            }
+            catch (ArgumentException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
